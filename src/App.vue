b6ba9e7a2f4dd20c5c8ddbe3644af3b70c28cf4c
Tom Scharstein <trscharstein@gmail.com>
2019-10-20 09:29:16 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- This is the root of your panel -->
+    <!-- Content should go inside #app -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue CLI panel" />
+
+    <!-- Utility component to handle context and flyout menus -->
+    <menus />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// You can access this App.vue file from any other component via `this.$root.$children[0]`
+// See `./components/HelloWorld.vue` for example of CSInterface and this.app
+
+// Create your own components and import them here
+import HelloWorld from "./components/HelloWorld.vue";
+import menus from "./components/menus.vue";
+
+// Dynamic CSS variables that automatically handle all app themes and changes:
+// https://github.com/Inventsable/starlette
+import starlette from "starlette";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    menus
+  },
+  data: () => ({
+    csInterface: null
+  }),
+  mounted() {
+    this.csInterface = new CSInterface();
+
+    starlette.init();
+  },
+  methods: {
+    dispatchEvent(name, data) {
+      var event = new CSEvent(name, "APPLICATION");
+      event.data = data;
+      this.csInterface.dispatchEvent(event);
+    },
+    loadScript(path) {
+      this.csInterface.evalScript(`$.evalFile('${path}')`);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Various helper styles to match application theme */
+@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
+:root {
+  --quad: cubic-bezier(0.48, 0.04, 0.52, 0.96);
+  --quart: cubic-bezier(0.76, 0, 0.24, 1);
+  --quint: cubic-bezier(0.84, 0, 0.16, 1);
+
+  background-color: var(--color-bg);
+  color: var(--color-default);
+  font-family: "Open Sans", sans-serif;
+  font-size: 10px;
+}
+
+#app::-webkit-scrollbar {
+  display: block;
+}
+body::-webkit-scrollbar {
+  width: 0px;
+}
+
+::-webkit-scrollbar {
+  background-color: var(--color-scrollbar);
+  width: var(--width-scrollbar-track);
+}
+::-webkit-scrollbar-thumb {
+  width: var(--width-scrollbar-track);
+  background: var(--color-scrollbar-thumb);
+  border-radius: var(--radius-scrollbar-thumb);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: var(--color-scrollbar-thumb-hover);
+}
+::-webkit-scrollbar-resizer {
+  display: none;
+  width: 0px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-button {
+  height: 0px;
+}
+::-webkit-scrollbar-corner {
+  display: none;
 }
 </style>
